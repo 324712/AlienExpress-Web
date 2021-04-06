749d1b5935777d1d721e4ae8b25d23297ffbcb60
Mitchel Jansen <mitchel@mexter.nl>
2021-04-06 17:45:03 +0200

    Added: Footer

---
+++ src/footer.php
@@ -0,0 +1,115 @@
+<div id="sub-footer">
+    <div class="uk-container">
+        <div class="uk-child-width-1-4@m" uk-grid>
+            <div>
+                <h6>Populair</h6>
+                <ul>
+                    <li>
+                        <a href="#">
+                            Onze docenten
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            Vacatures
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            Schoolvakanties
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <h6>Snel naar</h6>
+                <ul>
+                    <li>
+                        <a href="#">
+                            Veelgestelde vragen
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            Nieuws
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            Rondleidingen
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <h6>Praktisch</h6>
+                <ul>
+                    <li>
+                        <a href="#">
+                            Ziekmelden
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            Schoolkosten en DUO
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            Informatie voor ouders
+                        </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div>
+                <h6>Contactgegevens</h6>
+                <ul>
+                    <li>
+                        <a href="#">
+                            030 28 15 100
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            06 257 050 51 (WhatsApp)
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="#">
+                            info@alienexpress.nl
+                        </a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div id="footer">
+    <div class="uk-container">
+        <div class="uk-child-width-1-2@m" uk-grid>
+            <div>
+                <p>
+                    AlienExpress | Alle rechten voorbehouden © 2021
+                </p>
+            </div>
+
+            <div class="uk-text-right">
+                <p>
+                    website design door <a href="#">Mitchel Jansen en Rens Baart</a>
+                </p>
+            </div>
+        </div>
+    </div>
+</div>
